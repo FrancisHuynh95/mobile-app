dc102e70a199f2ff928a85cd01ef54513496a79f
FrancisHuynh95 <huynh_francis95@icloud.com>
2024-06-07 13:04:07 -0700

    created test page to test camera

--- my-app/app/(tabs)/test.tsx
+++ my-app/app/(tabs)/test.tsx
@@ -1,5 +1,9 @@
+import { useCameraPermission } from "react-native-vision-camera"
+
 
 export default function Test(){
+    const {hasPermission, requestPermission} = useCameraPermission()
+    
     return (
         null
       )
