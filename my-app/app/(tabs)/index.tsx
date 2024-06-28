f29da3c9c1500bfbe2ed20d977cf576083dcaaf0
FrancisHuynh95 <huynh_francis95@icloud.com>
2024-06-28 14:37:58 -0700

    added firebase info

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -4,6 +4,28 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC30S508bd9Btymu-QMTjnj5st8ChPGE1Q",
+  authDomain: "calorieburner-c2663.firebaseapp.com",
+  projectId: "calorieburner-c2663",
+  storageBucket: "calorieburner-c2663.appspot.com",
+  messagingSenderId: "47313775324",
+  appId: "1:47313775324:web:2907cb6001aec78b2ba219",
+  measurementId: "G-S8YVX663RX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
